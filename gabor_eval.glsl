2bba4e31fdcbb1c8a34242c2c4bff45cfbec7876
Matt Zucker <matt.zucker@gmail.com>
2018-07-01 17:55:37 -0400

    using scalar phi and vector h instead of vector phi and scalar h gave slightly better objective value (0.0130 after 3.5M iter vs 0.0136 after 3.5M) but didn't look quite as good; next try vector both again?

--- gabor_eval.glsl
+++ gabor_eval.glsl
@@ -25,11 +25,12 @@ void main() {
         vec4 phir = texelFetch(paramTexture, ivec2(pstart+1, param_idx), 0);
         vec4 hl   = texelFetch(paramTexture, ivec2(pstart+2, param_idx), 0);
 
+        
         float s = clamp(uvst.z, px, 2.0);
         float t = clamp(uvst.w, px, 4.0);
         float l = clamp(hl.w, 2.5*px, 4.0);
-        //vec3 h = clamp(hl.xyz, 0.0, 2.0);
-        float h = clamp(hl.x, 0.0, 2.0);
+        vec3 h = clamp(hl.xyz, 0.0, 2.0);
+        //float h = clamp(hl.x, 0.0, 2.0);
 
         float s2 = s*s;
         float t2 = t*t;
@@ -43,11 +44,12 @@ void main() {
 
         float f = 6.283185307179586 / l;
 
-        vec3 ck = cos(f*q.x + phir.xyz);
+        float ck = cos(f*q.x + phir.x);
+        //vec3 ck = cos(f*q.x + phir.xyz);
         float w = exp(-q.x*q.x/(2.*s2) - q.y*q.y/(2.*t2));
 
         vec3 g = h * w * ck;
-
+        
         accum += 0.5*g;
         
     }
